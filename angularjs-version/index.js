9d0353b476258cb2d80eb6d9ca4248d42a51b6e8
Chiranjivee Thakur <KH2273@kitspl.com>
2018-08-16 21:55:00 +0530

    Add angular version of multiplaceholder

--- angularjs-version/index.js
+++ angularjs-version/index.js
@@ -1,34 +1,33 @@
-var app = angular.module("multiLinePlaceHolder", []); 
-app.controller("myCtrl", function($scope) {
-    $scope.handleTextAreaEdit = function (myEvent) {
-        let length = myEvent.target.value.length;
-        if (length > 0) {
-            // myEvent.target.classList.remove("trans");
-            myEvent.target.classList.add("data-edits");
-        } else {
-            myEvent.target.classList.remove("data-edits");
-            // myEvent.target.classList.add("trans");
+angular.module("multiLinePlaceHolder", [])
+.controller("myCtrl", function($scope) {
+    $scope.textAreaInput = "";
+    $scope.handleTextAreaEdit = myEvent => {
+        if (!myEvent) {
+            return;
         }
+
+        let length = myEvent.target.value.length;
+        length > 0 ?
+            myEvent.target.classList.add("data-edits"):
+            myEvent.target.classList.remove("data-edits");
     };
 
     let scrollTop = 0;
-    $scope.mouseWheelHandler = function (myEvent) {
+    $scope.mouseWheelHandler = myEvent => {
         myEvent.deltaY < 0 ?
-		    document.getElementById("uniq").scrollTop = scrollTop-=10:
-		    document.getElementById("uniq").scrollTop = scrollTop+=10;
+            document.getElementById("uniq").scrollTop = scrollTop -= 10:
+            document.getElementById("uniq").scrollTop = scrollTop += 10;
     };
-});
-
-app.directive("ngWheel", ["$parse", function($parse) {
-    return function(scope, element, attr) {
-      var fn = $parse(attr.ngWheel);
-
-      element.bind("mousewheel", function(event) {
-        scope.$apply(function() {
-          fn(scope, {
-            $event: event
-          });
+})
+.directive("ngWheel", ["$parse", $parse => {
+    return (scope, element, attr) => {
+        const fn = $parse(attr.ngWheel);
+        element.bind("mousewheel", event => {
+            scope.$apply(() => {
+                fn(scope, {
+                    $event: event
+                });
+            });
         });
-      });
     };
 }]);
